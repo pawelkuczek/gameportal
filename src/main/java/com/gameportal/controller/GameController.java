e3bd5b3c419c32c7186be04964e7463b08579bb7
pawelkuczek <pawel.kuczek@vp.pl>
2026-04-02 18:24:49 +0200

    Add GameController with GET endpoints for games

---
+++ src/main/java/com/gameportal/controller/GameController.java
@@ -0,0 +1,31 @@
+package com.gameportal.controller;
+
+import com.gameportal.model.Game;
+import com.gameportal.service.GameService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/games")
+@RequiredArgsConstructor
+public class GameController {
+
+    private final GameService gameService;
+
+    @GetMapping
+    public ResponseEntity<List<Game>> getAllGames() {
+        return ResponseEntity.ok(gameService.getAllGames());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Game> getAllGames(@PathVariable Long id) {
+        return ResponseEntity.ok(gameService.getGameById(id));
+    }
+}
