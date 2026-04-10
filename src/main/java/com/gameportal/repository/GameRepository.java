4b5873a23ecfdb19179f0f9f7ddfd10315298825
pawelkuczek <pawel.kuczek@vp.pl>
2026-04-10 03:03:17 +0200

    Feat: extend GameRepository with JpaSpecificationExecutor for filtering support

--- src/main/java/com/gameportal/repository/GameRepository.java
+++ src/main/java/com/gameportal/repository/GameRepository.java
@@ -2,9 +2,10 @@ package com.gameportal.repository;
 
 import com.gameportal.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameRepository extends JpaRepository<Game, Long> {
+public interface GameRepository extends JpaRepository<Game, Long>, JpaSpecificationExecutor<Game> {
 
 }
